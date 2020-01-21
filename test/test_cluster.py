2d9bd190f209b059774ceed5748f31ae46836d43
Zachary Hoisington <zhoisington@pcd-5xdl40y-lt.ucsfmedicalcenter.org>
2020-01-20 16:22:12 -0800

    test

---
+++ test/test_cluster.py
@@ -0,0 +1,37 @@
+from hw2skeleton import cluster
+from hw2skeleton import io
+import os
+
+def test_similarity():
+    filename_a = os.path.join("data", "276.pdb")
+    filename_b = os.path.join("data", "4629.pdb")
+
+    activesite_a = io.read_active_site(filename_a)
+    activesite_b = io.read_active_site(filename_b)
+
+    # update this assertion
+    assert cluster.compute_similarity(activesite_a, activesite_b) == 0.0
+
+def test_partition_clustering():
+    # tractable subset
+    pdb_ids = [276, 4629, 10701]
+
+    active_sites = []
+    for id in pdb_ids:
+        filepath = os.path.join("data", "%i.pdb"%id)
+        active_sites.append(io.read_active_site(filepath))
+
+    # update this assertion
+    assert cluster.cluster_by_partitioning(active_sites) == []
+
+def test_hierarchical_clustering():
+    # tractable subset
+    pdb_ids = [276, 4629, 10701]
+
+    active_sites = []
+    for id in pdb_ids:
+        filepath = os.path.join("data", "%i.pdb"%id)
+        active_sites.append(io.read_active_site(filepath))
+
+    # update this assertion
+    assert cluster.cluster_hierarchically(active_sites) == []
