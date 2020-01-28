3899b11cf328c6756c270acb49b251ebb4bae4c7
Zachary Hoisington <zhoisington@pcd-5xdl40y-lt.ucsf.edu>
2020-01-28 15:39:05 -0800

    working clustering algorithm with example df

--- hw2skeleton/cluster.py
+++ hw2skeleton/cluster.py
@@ -1,6 +1,9 @@
 from .utils import Atom, Residue, ActiveSite
 import numpy as np
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+import math
+import pandas as pd
+import copy
 
 def compute_similarity(site_a, site_b):
     """
@@ -10,10 +13,17 @@ def compute_similarity(site_a, site_b):
     Output: the similarity between them (a floating point number)
     """
     similarity = 0.0
-    print("THIS LINE IS TO TEST THAT COMPUTE SIMILARITY IS RUNNING")
-    
 
-    # Fill in your code here!
+
+    print("THIS LINE IS TO TEST THAT COMPUTE SIMILARITY IS RUNNING")
+    x = site_a.residues[0].atoms[0].coords
+    y = site_b.residues[0].atoms[0].coords
+
+    #planning on computing all pairs and then summing up
+
+    distance = math.sqrt(sum([(a - b) ** 2 for a, b in zip(x, y)]))
+    print("EUCLIDEAN DISTANCE:",distance)
+    
     
     
     return similarity
@@ -31,13 +41,62 @@ def cluster_by_partitioning(active_sites):
     # Fill in your code here!
     compute_similarity(active_sites[0], active_sites[1])
 
-    k = 5 #arbitrary beginning value
+
+    df = pd.DataFrame({ #placeholder, DF will soon be similarity matrix
+    'x': [12, 20, 28, 18, 29, 33, 24, 45, 45, 52, 51, 52, 55, 53, 55, 61, 64, 69, 72],
+    'y': [39, 36, 30, 52, 54, 46, 55, 59, 63, 70, 66, 63, 58, 23, 14, 8, 19, 7, 24]
+    })
+    k = 3 #arbitrary beginning value
     centroids = {
-        i+1: [np.random.randint(0,130), np.random.randint(0,130)]
+        i+1: [np.random.randint(0,80), np.random.randint(0,80)] #select a value from dataframe for centroid
         for i in range(k)
-    }
+    } #randomly select three of the points to be the arbitrary centroids to start algorithm
     
-    #def assign(active_sites, centroids):
+
+    ##### BEGIN ASSIGNMENT TO CENTROID #####
+    def assignment(df, centroids): #we will assign each value in df to a centroid
+        for i in centroids.keys():
+        #euclidian distance calculation
+            df['distance_from_{}'.format(i)] = ( #distance of each point from the centroid
+              np.sqrt((df['x'] - centroids[i][0]) ** 2 + (df['y'] - centroids[i][1]) ** 2)
+        )
+        centroid_distance_cols = ['distance_from_{}'.format(i) for i in centroids.keys()]
+        df['closest'] = df.loc[:, centroid_distance_cols].idxmin(axis=1)
+        df['closest'] = df['closest'].map(lambda x: int(x.lstrip('distance_from_')))
+       
+        return df
+
+    df = assignment(df, centroids) #initialize the new dataframe with each sorted to a centroid
+
+    def update(k): #change the value of the centroids to be more representative of the cluster
+        for i in centroids.keys():
+            centroids[i][0] = np.mean(df[df['closest'] == i]['x'])
+            centroids[i][1] = np.mean(df[df['closest'] == i]['y'])
+        return k #k means takes the average x and y and moves the centroid closer 
+
+    centroids = update(centroids) #update the previous arbitrary centroids to be more accurate
+
+    ##### REPEAT ASSIGNMENT TO CENTROID #####
+
+    df = assignment(df, centroids)
+
+    ##### REPEAT UNTIL THERE ARE NO MORE CHANGES TO THE CLUSTERS #####
+    while True: 
+        closest_centroids = df['closest'].copy(deep=True)
+        centroids = update(centroids)
+        df = assignment(df, centroids)
+        if closest_centroids.equals(df['closest']):
+            break
+
+
+    fig = plt.figure(figsize=(5, 5))
+    plt.scatter(df['x'], df['y'])
+    for i in centroids.keys():
+         plt.scatter(*centroids[i])
+    plt.xlim(0, 80)
+    plt.ylim(0, 80)
+    plt.show()
+
     print(active_sites[0].name)
     print(active_sites[0].residues)
     print(active_sites[0].residues[0].atoms)
