3d1001ad6a01011401e503a56daec893109e7a77
Zachary Hoisington <zhoisington@pcd-5xdl40y-lt.ucsfmedicalcenter.org>
2020-01-29 20:26:32 -0800

    avg similarity is the x and y coords

--- hw2skeleton/cluster.py
+++ hw2skeleton/cluster.py
@@ -13,9 +13,7 @@ def compute_similarity(site_a, site_b):
     Output: the similarity between them (a floating point number)
     """
     similarity = 0.0
-
-
-    print("THIS LINE IS TO TEST THAT COMPUTE SIMILARITY IS RUNNING")
+    #print("THIS LINE IS TO TEST THAT COMPUTE SIMILARITY IS RUNNING")
     
     ## my GOAL is to have my similarity matrix compare how many amino acids
     ## residues of each protein have in common and normalize it to total they could have in common
@@ -23,14 +21,14 @@ def compute_similarity(site_a, site_b):
 
 
     AminoAcids = {'ALA': 'A', 'ARG': 'R', 'ASN': 'N', 'ASP': 'D', 'CYS': 'C',
-    'GLU': 'E', 'GLN': 'Q', 'GLY': 'G', 'HIS': 'H',
-    'ILE': 'I', 'LEU': 'L', 'LYS': 'K', 'MET': 'M', 'PHE': 'F',
-    'PRO': 'P', 'SER': 'S', 'THR': 'T', 'TRP': 'W',
-    'TYR': 'Y', 'VAL': 'V'} #shorten them to their 1 letter code to make this easier
+                  'GLU': 'E', 'GLN': 'Q', 'GLY': 'G', 'HIS': 'H', 'ILE': 'I', 
+                  'LEU': 'L', 'LYS': 'K', 'MET': 'M', 'PHE': 'F', 'PRO': 'P', 
+                  'SER': 'S', 'THR': 'T', 'TRP': 'W', 'TYR': 'Y', 'VAL': 'V'} 
+                  #shorten them to their 1 letter code to make this easier
 
 
     def AminoAcidSequence(active_site):
-        return [AminoAcids[x.type[0:3]] for x in active_site.residues]
+        return [AminoAcids[i.type[0:3]] for i in active_site.residues]
         #convert the input sequences so they are more workable
 
     seqA = AminoAcidSequence(site_a) #take the inputs to the compute similarity function
@@ -38,19 +36,23 @@ def compute_similarity(site_a, site_b):
     observedRes = set(seqA + seqB) #these are the residues that are observed in the inputs
     total = 0 #initializations
     common = 0
-    for res in observedRes:
-        inA = seqA.count(res)
+    for res in observedRes: #for each amino acid residue in observed residues
+        inA = seqA.count(res) #count the # of occurrences of the residue in each seq
         inB = seqB.count(res)
-        if inA < inB:
+        if inA < inB: #res more common in B than A it is added to total and common calc for A
             common += inA
             total += inB
-        else:
-            common += inA
-            total += inB
-    similarity = common / total
-    print("SIMILARITY IS:", similarity)
+        else: #opposite
+            common += inB
+            total += inA
+    similarity = (common / total)*100 # therefore it returns the # in common vs max possible in common, example:
+    # AND vs ANA
+    # A - 1 in common out of 2 max
+    # N - 1 in common out of 1
+    # D - 0 in common out of 1
+    # (1 + 1 + 0) / (2 + 1 + 1) = 2/4 = similarity of 0.5 or 50%
+    #print("SIMILARITY IS:", similarity)
     return similarity
-
 
 def cluster_by_partitioning(active_sites):
     """
@@ -60,21 +62,23 @@ def cluster_by_partitioning(active_sites):
     Output: a clustering of ActiveSite instances
             (this is really a list of clusters, each of which is list of
             ActiveSite instances)
+    My k-means clusering algorithm was adapted from: http://benalexkeen.com/k-means-clustering-in-python/
     """
     # Fill in your code here!
-    SMatrix = np.asarray([np.asarray([compute_similarity(i,j) for i in active_sites]) for j in active_sites])
-    
+    SMatrix = np.asarray([ #compile the similarity matrix by iterating all of the 
+                np.asarray([  #protein structures over each other
+                    compute_similarity(i,j) for i in active_sites]) 
+                         for j in active_sites])
 
-    df = pd.DataFrame({ #placeholder, DF will soon be similarity matrix
-    'x': [12, 20, 28, 18, 29, 33, 24, 45, 45, 52, 51, 52, 55, 53, 55, 61, 64, 69, 72],
-    'y': [39, 36, 30, 52, 54, 46, 55, 59, 63, 70, 66, 63, 58, 23, 14, 8, 19, 7, 24]
-    })
+
+    df = pd.DataFrame({'x': np.asarray(np.mean(SMatrix, axis = 0)),
+                       'y': np.asarray(np.mean(SMatrix, axis = 1))})
+
     k = 3 #arbitrary beginning value
     centroids = {
-        i+1: [np.random.randint(0,80), np.random.randint(0,80)] #select a value from dataframe for centroid
+        i+1: [np.random.randint(0,100), np.random.randint(0,100)] #select a value from dataframe for centroid
         for i in range(k)
     } #randomly select three of the points to be the arbitrary centroids to start algorithm
-
 
     ##### BEGIN ASSIGNMENT TO CENTROID #####
     def assignment(df, centroids): #we will assign each value in df to a centroid
@@ -123,9 +127,6 @@ def cluster_by_partitioning(active_sites):
 
 
     #####messing around with comparing the active site residues
-    print(active_sites[0].name)
-    print(len(active_sites[0].residues))
-    print(len(active_sites[1].residues))
     """
     for i in range(1):
         #print(len(active_sites[i].residues))
